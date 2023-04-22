47ab6228bcfa9bd46725903149d632d05a5dd180
Megha Banerjee <meghabanerjee025@gmail.com>
2023-04-23 01:20:57 +0530

    added observable and subject for post service

--- src/app/posts/post-list/post-list.component.ts
+++ src/app/posts/post-list/post-list.component.ts
@@ -1,19 +1,21 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnDestroy, OnInit } from '@angular/core';
 import { Post } from '../post.model';
 import { PostService } from '../post.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-post-list',
   templateUrl: './post-list.component.html',
   styleUrls: ['./post-list.component.css']
 })
-export class PostListComponent implements OnInit{
+export class PostListComponent implements OnInit, OnDestroy{
 
   @Input() posts: Post[]=[
     //   {title: 'First Post', content: 'This is the post content'},
     // {title: 'Second Post', content: 'This is the post content'},
     // {title: 'Third Post', content: 'This is the post content'}
   ];
+  private postsSub: Subscription = new Subscription;
 
   constructor(private postService: PostService){
 
@@ -21,5 +23,12 @@ export class PostListComponent implements OnInit{
 
   ngOnInit(){
     this.posts = this.postService.getPosts();
+    this.postsSub = this.postService.getPostUpdateListener().subscribe((posts) => {
+      this.posts = posts;
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.postsSub.unsubscribe();
   }
 }
--- src/app/posts/post.service.ts
+++ src/app/posts/post.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Post } from './post.model';
+import { Subject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -7,13 +8,22 @@ import { Post } from './post.model';
 export class PostService {
 
   private posts: Post[] = [];
+  private postsUpdated = new Subject<Post[]>();
+
   constructor() { }
 
   getPosts(){
     return [...this.posts];
   }
 
+  getPostUpdateListener(){
+    return this.postsUpdated.asObservable();
+  }
+
   addPost(post:Post){
     this.posts.push(post);
+    this.postsUpdated.next([...this.posts]);
   }
+
+
 }

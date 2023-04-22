41d0a63fb4b7a9af0f2faf205220056495b91641
Megha Banerjee <meghabanerjee025@gmail.com>
2023-04-22 20:37:51 +0530

    adding forms and validation for post fields

--- src/app/posts/post-create/post-create.component.ts
+++ src/app/posts/post-create/post-create.component.ts
@@ -1,5 +1,6 @@
 import { Component, EventEmitter, Output } from '@angular/core';
 import { Post } from '../post.model';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-post-create',
@@ -21,12 +22,17 @@ export class PostCreateComponent {
   //   this.newPost = postData.value;
   // }
 
-  onAddPost(){
+  onAddPost(form: NgForm){
 
   //  // alert("Post added !!");
   //   this.newPost = this.enteredContent;
 
-  const post = {title: this.enteredTitle, content: this.enteredContent};
+  // const post = {title: this.enteredTitle, content: this.enteredContent};
+
+  if(form.invalid){
+    return;
+  }
+  const post = {title: form.value.title, content: form.value.content};
 
   this.addPost.emit(post);
   }

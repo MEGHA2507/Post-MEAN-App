d7c09548809458bce5f1f80a37a9bc7f49af4ae9
MEGHA2507 <megha.banerjee07@gmail.com>
2026-02-21 04:50:45 +0530

    fixed the edit loading issue and also converted form from template driven to reactive to use more features and validation

--- src/app/post-create/post-create.ts
+++ src/app/post-create/post-create.ts
@@ -1,5 +1,5 @@
 import { ChangeDetectorRef, Component, EventEmitter, OnInit, Output } from '@angular/core';
-import { FormsModule, NgForm } from '@angular/forms';
+import { ReactiveFormsModule, NgForm, FormGroup, FormControl, Validators } from '@angular/forms';
 import {MatInputModule} from '@angular/material/input';
 import {MatButtonModule} from '@angular/material/button';
 import { MatCard } from '@angular/material/card';
@@ -11,7 +11,7 @@ import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
 
 @Component({
   selector: 'app-post-create',
-  imports: [CommonModule, MatProgressSpinnerModule, FormsModule, MatInputModule, MatButtonModule, MatCard],
+  imports: [CommonModule, MatProgressSpinnerModule, ReactiveFormsModule, MatInputModule, MatButtonModule, MatCard],
   templateUrl: './post-create.html',
   styleUrls: ['./post-create.scss'],
 })
@@ -25,7 +25,10 @@ export class PostCreate implements OnInit{
   private postId:any = null;
   post!:Post;
   postEditData = false;
-isLoading = false;
+  isLoading = false;
+
+  postForm!: FormGroup;
+
   constructor(private postsService: PostsService,
     public route: ActivatedRoute,
     private router: Router,
@@ -34,15 +37,24 @@ isLoading = false;
 
   }
 
+  createPostForm(){
+    this.postForm = new FormGroup({
+      postTitle: new FormControl(null, {validators: [Validators.required, Validators.minLength(3)]}),
+      postContent: new FormControl(null, {validators: [Validators.required]})
+    })
+  }
+
   ngOnInit(): void {
+    this.isLoading = false;
+    this.createPostForm();
     this.route.paramMap.subscribe((res) => {
       if(res.has('id')){
         this.mode = 'edit';
         this.postId = res.get('id');
-        this.isLoading = true;
+        
         this.postsService.getPost(this.postId).subscribe((res: any) => {
           if(res){
-            this.isLoading = false;
+           
             console.log(res);
             this.post = {
               id: res._id,
@@ -50,8 +62,14 @@ isLoading = false;
               postContent: res.postContent
             }
             this.postEditData = true;
+             this.isLoading = false;
           }
-         
+
+          this.postForm.setValue({
+            postTitle: this.post.postTitle,
+            postContent: this.post.postContent
+          });
+
         });
       }else{
         this.mode = 'create';
@@ -67,8 +85,8 @@ isLoading = false;
     })
   }
 
-  onSavePost(form: NgForm) {
-    if(form.invalid){
+  onSavePost() {
+    if(this.postForm.invalid){
       return;
     }
     //console.log(form.value)
@@ -79,14 +97,14 @@ isLoading = false;
   //  this.postCreated.emit(post);
   this.isLoading = true;
   if(this.mode === 'create'){
-      this.postsService.addPost('',form.value.postTitle, form.value.postContent);
+      this.postsService.addPost('', this.postForm.value.postTitle, this.postForm.value.postContent);
       this.isLoading = false;
   }else{
-    this.postsService.editPost(this.postId, form.value.postTitle, form.value.postContent);
+    this.postsService.editPost(this.postId, this.postForm.value.postTitle, this.postForm.value.postContent);
     this.isLoading = false;
   }
 
-   form.resetForm();
+   this.postForm.reset();
    
   }
 }

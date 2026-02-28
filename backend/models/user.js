09045f02b90e8d8446a42aa3b93719d20c6cf63d
MEGHA2507 <megha.banerjee07@gmail.com>
2026-02-28 20:11:37 +0530

    user model user routes and packges for password decryption bcrypt added

--- backend/models/user.js
+++ backend/models/user.js
@@ -1,14 +1,14 @@
-const { required } = require('@angular/forms/signals');
 const mongoose = require('mongoose');
 
-const uniqueValidator = require("mongoose-unique-validator");
-
-const userSchema = mongoose.Schema({
-   email: { type: String, required: true, unique: true},
-   password: {type: String, required: true}
+const userSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    required: true   // ❗ remove unique here
+  },
+  password: {
+    type: String,
+    required: true
+  }
 });
 
-userSchema.plugin(uniqueValidator);
-
 module.exports = mongoose.model('User', userSchema);
-
--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,9 +1,26 @@
 const express = require("express");
+const bcrypt = require("bcrypt");
+const UserModel = require('../models/user'); // relative path to user.js
 const router = express.Router();
 
 router.post("/signup", (req, res, next) => {
-    
-});
+    bcrypt.hash(req.body.password, 10).then((hashedData) => {
+        const user = new UserModel({
+            email: req.body.email,
+            password: hashedData
+        });
 
+        user.save()
+        .then((result) => {
+            res.status(201).json({
+                message: "User created successfully !!",
+                result: result
+            });
+        })
+        .catch((err) => {
+            res.status(500).json({ error: err });
+        });
+    });
+});
 
 module.exports = router;

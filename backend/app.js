fe717be440ab585dc5754464654d5f71ed5dba02
MEGHA2507 <megha.banerjee07@gmail.com>
2026-03-14 16:25:13 +0530

    post backend deploymnet chnages

--- backend/app.js
+++ backend/app.js
@@ -8,7 +8,7 @@ const userRoutes = require('./routes/user');
 
 const app = express();
 
-mongoose.connect("mongodb+srv://meghab:"+ process.env.MONGO_ATLAS_PW +"@cluster0.h4kbubm.mongodb.net/")
+mongoose.connect("mongodb+srv://meghab:"+ process.env.MONGO_ATLAS_PW +"@cluster0.h4kbubm.mongodb.net/node-angular?retryWrites=true&w=majority")
 .then(() => {
     console.log('Connected to database!');
 })
@@ -18,13 +18,17 @@ mongoose.connect("mongodb+srv://meghab:"+ process.env.MONGO_ATLAS_PW +"@cluster0
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
-app.use("/images", express.static(path.join("backend/images")));
+app.use("/images", express.static(path.join(__dirname, "images")));
 
 app.use((request, response, next) => {
     response.setHeader("Access-Control-Allow-Origin","*");
     response.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
     response.setHeader("Access-Control-Allow-Methods", "GET, POST, PATCH, PUT, DELETE, OPTIONS");
     next();
+});
+
+app.get("/", (req, res) => {
+  res.send("API running");
 });
 
 app.use('/api/posts', postRoutes);
